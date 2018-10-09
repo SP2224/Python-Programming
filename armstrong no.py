ecc7c36e645de7a7c9db261ec52d80ba5def084d
Ankitvit2509 <43986396+Ankitvit2509@users.noreply.github.com>
2018-10-09 15:16:30 +0530

    Add files via upload

---
+++ armstrong no.py
@@ -0,0 +1,12 @@
+x=int(input("enter number"))
+i=num
+result=0
+n=len(str(i))
+while  (i>=0)
+       digit=i%10
+       result=result+digit**n
+       i=i//10
+    if result=num:
+       print(""armstrong number")
+    else:
+       print("not armstrong number")
