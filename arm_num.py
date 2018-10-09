c9cda20e64749ad19b051ff15997920dea369b7f
Ankitvit2509 <43986396+Ankitvit2509@users.noreply.github.com>
2018-10-09 15:46:12 +0530

    Add files via upload

---
+++ arm_num.py
@@ -0,0 +1,13 @@
+def arm(x)
+    t=x
+    sum1=0  
+    while(t>0):
+      d=t%10
+      sum1+=d**3
+      t=t//10
+    if sum1==x:
+       print('A')
+    else:
+       print("not armstrong no")
+x=int(input("enter number"))
+Arm(x)    
